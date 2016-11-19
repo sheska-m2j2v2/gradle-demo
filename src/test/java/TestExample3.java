c515c4307caee23035a6ec2e8b1fccf79d7c5421
Brent Laster <nfjsuser1@gmail.com>
2016-11-19 16:10:04 -0500

    add new test files

---
+++ src/test/java/TestExample3.java
@@ -0,0 +1,12 @@
+// File: src/test/java/TestExample3.java
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestExample3 {
+
+  @Test public void example3() {
+
+	Assert.assertEquals("This is test 3", "This is test 3");
+  }
+}
---
+++ src/test/java/TestExample4.java
@@ -0,0 +1,12 @@
+// File: src/test/java/TestExample4.java
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestExample4 {
+
+  @Test public void example4() {
+
+	Assert.assertEquals("test 4", "test 4");
+  }
+}
